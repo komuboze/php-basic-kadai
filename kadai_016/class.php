4a2c47d5ac6a983af8758c58a54e8d96847d769f
komuboze <158114956+komuboze@users.noreply.github.com>
2024-02-17 11:35:08 +0900

    課題提出

---
+++ kadai_016/class.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <title>Document</title>
+</head>
+<body>
+  <p>
+    <?php
+    class Food {
+      private $name;
+      private $price;
+      public function __construct(string $name, int $price) {
+        $this->name=$name;
+        $this->price=$price;
+      }
+      public function show_price()
+      {
+          echo $this->price;
+      }
+    }
+ 
+    class Animal {
+      private $name;
+      private $height;
+      private $weight;
+      public function __construct(string $name, int $height, int $weight) {
+        $this->name=$name;
+        $this->height=$height;
+        $this->weight=$weight;
+      }      
+      public function show_height()
+      {
+          echo $this->height;
+      }
+    }
+    $food = new Food('potato', 250);
+    print_r($food);
+    echo'<br>';
+    $animal = new Animal('dog', 60, 5000);
+    print_r($animal);
+    echo'<br>';
+    $food->show_price();
+    echo'<br>';
+    $animal->show_height();
+
+ 
+    ?>
+  </p>
+</body>
+</html>
